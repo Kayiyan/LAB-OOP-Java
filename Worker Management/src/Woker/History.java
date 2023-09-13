6b9ee3673615b05ed8cec1816c5676586bd20d4c
Kayiyan <126185640+Kayiyan@users.noreply.github.com>
2023-09-13 21:32:19 +0700

    Add files via upload

---
+++ Worker Management/src/Woker/History.java
@@ -0,0 +1,30 @@
+package Woker;
+
+public class History extends Worker {
+    private String Status;
+    private String Date;
+
+    public History( String id, String name, int age, double salary, String workLocation,String Status, String Date) {
+        super(id, name, age, salary, workLocation);
+        this.Status = Status;
+        this.Date = Date;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+    
+    
+}
