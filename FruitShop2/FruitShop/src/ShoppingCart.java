0a992b7047ae30e113deb333a1b25097d942d989
Kayiyan <126185640+Kayiyan@users.noreply.github.com>
2023-10-01 21:53:17 +0700

    Add files via upload

---
+++ FruitShop2/FruitShop/src/ShoppingCart.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<Fruit> items = new ArrayList<>();
+    private double totalPrice = 0;
+
+//    public void addItem(Fruit fruit, int quantity) {
+//        fruit.setQuantity(quantity);
+//        items.add(fruit);
+//        totalPrice += fruit.getTotalPrice();
+//    }
+
+    public List<Fruit> getItems() {
+        return items;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setItems(List<Fruit> items) {
+        this.items = items;
+        totalPrice = items.stream().mapToDouble(Fruit::getTotalPrice).sum();
+    }
+}
