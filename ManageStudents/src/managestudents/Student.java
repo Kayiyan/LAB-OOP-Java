ce7166e127747430805bf772402ee6caa2305925
Kayiyan <126185640+Kayiyan@users.noreply.github.com>
2023-10-10 22:37:37 +0700

    Add files via upload

---
+++ ManageStudents/src/managestudents/Student.java
@@ -0,0 +1,51 @@
+package managestudents;
+public class Student {
+    private String id;
+    private String studentName;
+    private String semester;
+    private String courseName;
+
+    public Student(String id, String studentName, String semester, String courseName) {
+        this.id = id;
+        this.studentName = studentName;
+        this.semester = semester;
+        this.courseName = courseName;
+    }
+    
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentsName) {
+        this.studentName = studentsName;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    
+    public void print(){
+        System.out.printf("%-15%-15%-15\n",studentName,semester,courseName);
+    }
+}
